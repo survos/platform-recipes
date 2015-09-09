d2b2ac1f336189d7dec80032198b9faa773235a7
Phill <phill@starkingdom.co.uk>
2015-09-09 21:29:26 +0100

    Updated if...else and added comments

--- R/getJobs.R
+++ R/getJobs.R
@@ -1,11 +1,24 @@
-firstTime <- 1
-if (firstTime) {
-  install.packages('devtools')
+if(!require("devtools")){
+  # If devtools fails with zero exit status due to unable to install xml2 library, 
+  # check libxml2-dev is installed on local machine
+  install.packages("devtools")
+  library("devtools")
   devtools::install_github("survos/platform-api-r")
-}
+  library("survos")
 
-library("survos")
+} else {
+  library("devtools")
+  devtools::install_github("survos/platform-api-r")
+  library("survos")
+  }
 
-loginSurvos(username = "YourUserNameHere", password = "YourPasswordHere", style = "POST")
+library("RCurl")
+library("jsonlite")
+library("httr")
 
-jobsFrame <- jobs(projectCode = "nyu_demo", page = "1")
+loginSurvos(username="YourUsername", password="YourPassword")
+
+jobsOut <- jobs(projectCode="nyu_demo")
+
+# A list of all job ids for project code "nyu_demo" now in this variable
+jobsOut$id
