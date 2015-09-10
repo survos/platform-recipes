43a64f44dbb39c9bf671488124b4eacffe4ddb95
Phill <phill@starkingdom.co.uk>
2015-09-11 00:53:08 +0200

    Update getJobs.R
    Updated with parameters.R requirement. Added users and assignments output examples.

--- R/getJobs.R
+++ R/getJobs.R
@@ -1,3 +1,4 @@
+# Check for the availability of devtools. If not installed, do so
 if(!require("devtools")){
   # If devtools fails with zero exit status due to unable to install xml2 library, 
   # check libxml2-dev is installed on local machine
@@ -5,20 +6,34 @@ if(!require("devtools")){
   library("devtools")
   devtools::install_github("survos/platform-api-r")
   library("survos")
-
+ 
+# Force install newest version of survos package.
 } else {
   library("devtools")
   devtools::install_github("survos/platform-api-r")
   library("survos")
-  }
+}
 
+# Load additional libraries. These should all have been installed with the survos package if not previously.
 library("RCurl")
 library("jsonlite")
 library("httr")
+library("dplyr")
 
-loginSurvos(username="YourUsername", password="YourPassword")
+# Load external file containing username, password and API endpoint data. File must be saved in active working directory
+source("parameters.R")
 
+# Login
+loginSurvos(username, password)
+
+# Extract jobs data
 jobsOut <- jobs(projectCode="nyu_demo")
 
 # A list of all job ids for project code "nyu_demo" now in this variable
 jobsOut$id
+
+# A data frame containing all user data, from across 29 pages
+users <- users()
+
+# Returns all assignment data associated with job_id 163
+assignments <- assignments(163)
