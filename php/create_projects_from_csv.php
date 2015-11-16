4c7630bc0d50bab21043b4e2943f6805dd381df9
Tac Tacelosky <tacman@gmail.com>
2015-11-16 17:52:25 -0500

    attempt at import projects script

--- php/create_projects_from_csv.php
+++ php/create_projects_from_csv.php
@@ -4,6 +4,12 @@ require __DIR__.'/vendor/autoload.php';
 use Survos\Client\SurvosClient;
 use Survos\Client\Resource\ProjectResource;
 
+$reader = new \EasyCSV\Reader('new_projects.csv');
+
+while ($row = $reader->getRow()) {
+    print_r($row);
+}
+
 $config = json_decode(file_get_contents(__DIR__.'/config.json'), true);
 $client = new SurvosClient($config['endpoint']);
 if (!$client->authorize($config['username'], $config['password'])) {
@@ -19,4 +25,10 @@ $resource->save(
         'timezone_id' => 1,
     ]
 );
+
+$reader = new \EasyCSV\Reader('new_members.csv');
+
+while ($row = $reader->getRow()) {
+    print_r($row);
+}
 
