7797641135cfd2f48494fbefbd8d599e0a6882d6
Piotr Grochowski <piogrek@gmail.com>
2015-11-20 18:22:20 +0000

    updating task summary

--- php/Command/SummaryTasksCommand.php
+++ php/Command/SummaryTasksCommand.php
@@ -25,8 +25,14 @@ class SummaryTasksCommand extends BaseCommand
             ->addOption(
                 'project-code',
                 null,
-                InputOption::VALUE_REQUIRED,
+                InputOption::VALUE_OPTIONAL,
                 'Project code'
+            )->addOption(
+                'limit',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                '',
+                100
             );
     }
 
@@ -36,8 +42,14 @@ class SummaryTasksCommand extends BaseCommand
         parent::execute($input, $output);
         $isVerbose = $output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE;
         $projectCode = $input->getOption('project-code');
+        $limit = $input->getOption('limit');
 
         $tasksResource = new TaskResource($this->sourceClient);
+
+        $params = [];
+        if ($projectCode) {
+            $params['project_code'] = $projectCode;
+        }
 
         $page = 0;
         $perPage = 10;
@@ -46,19 +58,22 @@ class SummaryTasksCommand extends BaseCommand
         $data = [];
         $no = 1;
         while ($page < $maxPages) {
-            $tasks = $tasksResource->getList(++$page, $perPage, $criteria, [], [], ['project_code' => $projectCode]);
+            $tasks = $tasksResource->getList(++$page, $perPage, $criteria, [], [], $params);
             $maxPages = $tasks['pages'];
             // if no items, return
-            if (!count($tasks['items']) || !$tasks['total']) {
+            if (!count($tasks['items']) || !$tasks['total'] || ($limit > 0 && $no > $limit)) {
                 break;
             }
 
             foreach ($tasks['items'] as $key => $task) {
 
                 $data[] = [
-                    '#' => $no,
-                    'task_id'                   => $task['id'],
-                    'code'                   => isset($task['code']) ? $task['code'] :'-',
+                    '#'                => $no,
+                    'task_id'          => $task['id'],
+                    'code'             => isset($task['code']) ? $task['code'] : '-',
+                    'project_code'     => isset($task['project_code']) ? $task['project_code'] : '-',
+                    'assignment_count' => isset($task['assignment_count']) ? $task['assignment_count'] : '-',
+                    'task_status_code' => isset($task['task_status_code']) ? $task['task_status_code'] : '-',
                 ];
                 $no++;
             }
@@ -66,7 +81,16 @@ class SummaryTasksCommand extends BaseCommand
 
         $table = new Table($output);
         $table
-            ->setHeaders(['#','task_id','code'])
+            ->setHeaders(
+                [
+                    '#',
+                    'task_id',
+                    'code',
+                    'project_code',
+                    'assignment_count',
+                    'task_status_code',
+                ]
+            )
             ->setRows($data);
         $table->render();
     }
